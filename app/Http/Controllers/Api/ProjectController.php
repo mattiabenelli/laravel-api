c9d17a8da967dbf929a480380c89789da9ed87e0
mattiabenelli <benellimattia0@gmail.com>
2023-03-11 11:41:52 +0100

    milestone 1

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+        $projects = Project::all();
+
+        return response()->json([
+            'success' => true,
+            'projects' => $projects
+        ]);
+    }
 }
